5a9b981cf0ea8243334a1dff22aa1dfaf9959af0
ckouffsyrup <ckouffsyrup@gmail.com>
2026-08-27 19:59:45 -0500

    Delete v4_4_8_multicolor_capable.sql

--- v4_4_8_multicolor_capable.sql
+++
@@ -1,5 +0,0 @@
--- PrintBook v4.4.8
--- Lets the owner explicitly decide which products accept custom multicolor requests.
-
-alter table public.prints
-  add column if not exists multicolor_capable boolean not null default false;
