3b5a4e2d83a1786ede69d64895c1e69f033dbe8c
ckouffsyrup <ckouffsyrup@gmail.com>
2026-08-30 19:46:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2711,9 +2711,7 @@ safeUiInit("startup-storefront-v2",()=>{
   $("storefrontShopNavBtn")?.addEventListener("click",scrollToShop);
   $("storefrontShopHeroBtn")?.addEventListener("click",scrollToShop);
   $("storefrontViewAllBtn")?.addEventListener("click",scrollToShop);
-  $("storefrontCustomBrowseBtn")?.addEventListener("click",scrollToShop);
   $("storefrontOrdersNavBtn")?.addEventListener("click",openFindCustomerOrder);
-  $("storefrontCustomHeroBtn")?.addEventListener("click",scrollToShop);
   $("storefrontMobileSearchBtn")?.addEventListener("click",scrollToShop);
   $("storefrontMobileOrdersBtn")?.addEventListener("click",openFindCustomerOrder);
   $("storefrontMobileShareBtn")?.addEventListener("click",()=>$("customerShareStoreHeroBtn")?.click());
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="printbook-v5.8.5-mobile-header-fix";
+const CACHE="printbook-v5.8.6-no-request-custom";
 const CORE_ASSETS=["./","./index.html","./styles.css","./storefront-v55.css","./app.js","./store-qr.png"];
 
 self.addEventListener("install",event=>{
