5d07d146391ada3bcb01d79fd62c95588492341c
ckouffsyrup <ckouffsyrup@gmail.com>
2026-08-30 19:24:20 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="printbook-v5.8.3-featured-admin-fix";
+const CACHE="printbook-v5.8.4-mobile-hero-hotfix";
 const CORE_ASSETS=["./","./index.html","./styles.css","./storefront-v55.css","./app.js","./store-qr.png"];
 
 self.addEventListener("install",event=>{
